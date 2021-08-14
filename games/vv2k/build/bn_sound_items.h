f4989d66a216a0a72c420b1fa87cab014bc14f57
Ethan Hill <ethan.hill1999@outlook.com>
2021-08-13 23:00:35 -0500

    p

--- games/vv2k/build/bn_sound_items.h
+++ games/vv2k/build/bn_sound_items.h
@@ -5,11 +5,11 @@
 
 namespace bn::sound_items
 {
-    constexpr inline sound_item heymaple(3);
-    constexpr inline sound_item shortclip(4);
-    constexpr inline sound_item ui_sfx01(5);
-    constexpr inline sound_item ui_sfx02(6);
-    constexpr inline sound_item ui_sfx03(7);
+    constexpr inline sound_item heymaple(2);
+    constexpr inline sound_item shortclip(3);
+    constexpr inline sound_item ui_sfx01(4);
+    constexpr inline sound_item ui_sfx02(5);
+    constexpr inline sound_item ui_sfx03(6);
 }
 
 #endif
--- games/vv2k/build/bn_sound_items_info.h
+++ games/vv2k/build/bn_sound_items_info.h
@@ -8,11 +8,11 @@
 namespace bn::sound_items_info
 {
     constexpr inline pair<sound_item, string_view> array[] = {
-        make_pair(sound_item(3), string_view("heymaple")),
-        make_pair(sound_item(4), string_view("shortclip")),
-        make_pair(sound_item(5), string_view("ui_sfx01")),
-        make_pair(sound_item(6), string_view("ui_sfx02")),
-        make_pair(sound_item(7), string_view("ui_sfx03")),
+        make_pair(sound_item(2), string_view("heymaple")),
+        make_pair(sound_item(3), string_view("shortclip")),
+        make_pair(sound_item(4), string_view("ui_sfx01")),
+        make_pair(sound_item(5), string_view("ui_sfx02")),
+        make_pair(sound_item(6), string_view("ui_sfx03")),
     };
 
     constexpr inline span<const pair<sound_item, string_view>> span(array);
